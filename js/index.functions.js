6592cefd12aca824b96c6dae5c3e925e14df21b0
BealeStBluesBoy <gustavo.cargnelutti@outlook.com>
2019-04-25 23:59:32 -0300

    Alivianar carga para dispositivos moviles
    * Reducir resultados de la API a la mitad
    * No precargar audio
    
    Usualmente los resultados significativos estan por debajo de los 100

--- js/index.functions.js
+++ js/index.functions.js
@@ -15,8 +15,7 @@ function buscar() {
 
     $("#recientes").empty();
 
-    // La API de iTunes solo permite hasta 200 resultados
-    var searchUrl = "https://itunes.apple.com/search?media=music&entity=song&limit=200&term=" + $("#busqueda").val().replace(" ", "+");
+    var searchUrl = "https://itunes.apple.com/search?media=music&entity=song&limit=100&term=" + $("#busqueda").val().replace(" ", "+");
 
     $("#resultados").html(
         Mustache.render($("#cargando").html())
