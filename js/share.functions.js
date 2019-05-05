bb8a2190fe34cabefc3c08ba59764ead3c10b128
Gustavo Cargnelutti <gustavo.cargnelutti@outlook.com>
2019-05-05 12:33:32 -0300

    Preparar formulario al cargar la pagina
    Importar plantilla reducida de la pagina prncipal

--- js/share.functions.js
+++ js/share.functions.js
@@ -1,3 +1,12 @@
+function prepararForma() {
+    var searchUrl = "https://itunes.apple.com/lookup?id=" + window.sessionStorage.getItem("compartido");
+    $.getJSON(searchUrl, function(response) {
+        $("#resultado").append(
+            Mustache.render($("#cancionTmpl").html(), response.results[0])
+        );
+    });
+}
+
 //Expresi�n para validar un correo electr�nico
 var Expr = document.getElementById("Expr").value.trim();
         var Expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
