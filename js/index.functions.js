8168a2b36ad0199a518a46e1baaaa4bd1ee645fc
Florencia <florencia.rodriguez.a@hotmail.com>
2019-05-04 23:07:16 -0300

    Cambios en el share  y js

--- js/index.functions.js
+++ js/index.functions.js
@@ -193,3 +193,8 @@ function end(elemento) {
     $(elemento).trigger("pause");
     $(elemento).closest("li[name='resultado']").find("img[name='audCtrl']").attr("src", "res/play.svg");
 }
+
+
+//Expresi�n para validar un correo electr�nico
+var Expr = document.getElementById("Expr").value.trim();
+        var Expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
