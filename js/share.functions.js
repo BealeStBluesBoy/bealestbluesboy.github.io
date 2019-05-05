4241565b3b676b58846c385ad386d067061d97d5
Gustavo Cargnelutti <gustavo.cargnelutti@outlook.com>
2019-05-05 13:05:03 -0300

    Remover expresion regular
    El propio formulario chequea el mail antes de ejecutar enviar()

--- js/share.functions.js
+++ js/share.functions.js
@@ -8,12 +8,9 @@ function prepararForma() {
 }
 
 function enviar() {
-    //Expresion para validar un correo electronico (RFC 5322 Official Standard)
-    var expr = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-    if (expr.test($("#mail").val())) {
-        var mail = document.createElement("a");
-        mail.href = "mailto:" + $("#mail").val();
-        mail.href += "?subject=Musica&body=" + encodeURIComponent(
+    var mail = document.createElement("a");
+    mail.href = "mailto:" + $("#mail").val();
+    mail.href += "?subject=Musica&body=" + encodeURIComponent(
 
 `Hola ${ $("#nomApe").val() }
 
@@ -21,8 +18,8 @@ Te recomiendo esta cancion: "${ $("#nombre").text() }", de ${ $("#artista").text
 
 Saludos!
 `
-        );
-        mail.click();
-    }
+    );
+    mail.click();
+
     return false; // Evita que la pagina recargue
 }
